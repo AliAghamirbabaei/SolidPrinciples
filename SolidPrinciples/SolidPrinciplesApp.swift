14b7da8bf28ce7ffe8e5a3c2cc9c5c64d6f548a4
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-02-17 23:08:33 +0330

    Initial Commit

---
+++ SolidPrinciples/SolidPrinciplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidPrinciplesApp.swift
+//  SolidPrinciples
+//
+//  Created by Ali Aghamirbabaei on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SolidPrinciplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
